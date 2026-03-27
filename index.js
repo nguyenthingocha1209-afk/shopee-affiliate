cbd427c884313baf87567a3f102e8b83f29ff4e6
nguyenthingocha1209-afk <nguyenthingocha1209@gmail.com>
2026-03-27 17:01:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
 
-const affiliateLink = "https://s.shopee.vn/AUpb4ZviyZ";
+const affiliateLink = "https://s.shopee.vn/xxxxx"; // link của bạn
 
-app.get("/", (req, res) => {
+// 👉 HIỂN THỊ WEB
+app.use(express.static("public"));
+
+// 👉 NÚT MUA MỚI CHUYỂN LINK
+app.get("/go", (req, res) => {
   res.redirect(affiliateLink);
 });
 
 const PORT = process.env.PORT || 8080;
-
 app.listen(PORT, () => {
-  console.log("Server chạy tại port " + PORT);
+  console.log("Server chạy");
 });
